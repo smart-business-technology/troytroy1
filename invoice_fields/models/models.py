b20a391895089b5c05d9a8d276ad77941da69893
smart-business-technology <81749573+smart-business-technology@users.noreply.github.com>
2022-01-01 23:28:04 +0300

    Add files via upload

--- invoice_fields/models/models.py
+++ invoice_fields/models/models.py
@@ -8,5 +8,13 @@ class invoice_fields(models.Model):
 
 
     driver_name = fields.Char("Driver Name")
+    anfas_cost = fields.Float(string="التكلفه", required=False, )
+
     # carpenter_name = fields.Char("Carpenter Name")
     carpenter_name = fields.Many2one(comodel_name="res.partner", string="Carpenter Name", required=False, )
+    total_real_cost = fields.Float(string="Total Real price", required=False,
+                                   )
+class AccountMoveLineInherit(models.Model):
+
+    _inherit = 'account.move.line'
+    real_price_unit = fields.Float('Real Unit Price', required=True, digits='Product Price', default=0.0)
